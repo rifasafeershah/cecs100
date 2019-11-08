5c6a463fd40d154a09e0e60e7aca59b9851481f0
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-08 08:21:58 -0800

    Create exercise1.py

---
+++ final/exercise1.py
@@ -0,0 +1,8 @@
+
+integer = int(input("Enter a Positive Integer greater than 1 : "))
+
+while integer < 1:
+
+    print ("Your input is invalid! try again")
+
+    integer = int(input("Enter a Positive Integer greater than 1 : "))
