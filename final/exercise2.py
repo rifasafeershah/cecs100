f1c6310a7d6d1fb3f28699b28fd1a45bb136cda1
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-08 08:22:26 -0800

    Create exercise2.py

---
+++ final/exercise2.py
@@ -0,0 +1,14 @@
+
+def main():
+
+    increase_years()
+
+def increase_years():
+
+    years = int(input("Enter the number of years: "))
+    
+    increase = 1.8
+
+    rise = increase*years
+
+main()
