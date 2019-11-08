7645bf93aaed778889d1973e89d6765b638cf14e
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-08 08:24:37 -0800

    Create lab2.py

---
+++ labs/lab2.py
@@ -0,0 +1,30 @@
+
+cost_of_meal = 50
+
+cost_with_tip1 = (cost_of_meal * 15/100)
+
+cost_with_tip2 = (cost_of_meal * 18/100)
+
+cost_with_tip3 = (cost_of_meal * 20/100)
+
+print ("15% Tip Will Be")
+print (cost_with_tip1) #15% tip
+
+print ("total cost of meal will be")
+print (cost_of_meal + cost_with_tip1)
+
+print ("18% Tip Will Be")
+print (cost_with_tip2) #18% tip
+
+print ("total cost of meal will be")
+print (cost_of_meal + cost_with_tip2)
+
+print ("20% Tip Will Be")
+print (cost_of_meal * 20/100) #20% tip
+
+print ("total cost of meal will be")
+print (cost_of_meal + cost_of_meal * 20/100)
+
+cost_of_meal = 100
+
+print (cost_of_meal * 25/100)
