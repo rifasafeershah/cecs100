6ea112e0ba67ccacd13e8d06b67e13f5d5cc3d56
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-08 08:18:09 -0800

    Create lab3.py

---
+++ labs/lab3.py
@@ -0,0 +1,11 @@
+name = input ("What is your name? ")
+
+age = int ( input ("What is your age? "))
+
+age_in_2020 = (age+3)
+
+print ("Name:",name)
+
+print ("Age:",age)
+
+print ("Age in 2020:",age_in_2020)
